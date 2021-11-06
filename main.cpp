438b0983d352629343033fc4117d43fc9535d41e
Casey F Riley <innerforest7@gmail.com>
2021-11-06 06:06:50 +0000

    add pseudo code

--- main.cpp
+++ main.cpp
@@ -51,85 +51,85 @@ arm.flex();                             // 3)
 arm.rotateCounterClockwise();           // 4) demonstrates CamelCase
 
 
-//  1)
+//  speaker //  1)
 //  Noun:
-//  action 1:
+//  action 1: the speaker blasts
 speaker.blastMusic();
-//  action 2:
+//  action 2: the speaker powers off
 speaker.powerOff();
-//  action 3:
+//  action 3: the speaker changes volume
 speaker.changeVolume();
-//  2)
+//  fan. 2)
 //  Noun:
-//  action 1:
+//  action 1: the fan turns on
 fan.on();
-//  action 2:
+//  action 2: the fan turns off
 fan.off();
-//  action 3:
+//  action 3: the fan rotates
 fan.rotate();
-//  3)
+//  monitor  3)
 //  Noun:
-//  action 1:
+//  action 1: the monitor selects HDMI
 monitor.selectHDMI();
-//  action 2:
+//  action 2: the monitor selects Bluetooth
 monitor.selectBluetooth();
-//  action 3:
+//  action 3: the monitor selects USBC
 monitor.selectUSBC();
-//  4)
+//  cat  4)
 //  Noun:
-//  action 1:
+//  action 1: the cat runs
 cat.run();
-//  action 2:
+//  action 2: the cat jumps
 cat.jump();
-//  action 3:
+//  action 3: the cat sleeps
 cat.sleep();
-//  5)
-//  Noun:
+//  dog  5)
+//  Noun: the dog barks
 //  action 1:
 dog.bark();
-//  action 2:
+//  action 2: the dog digs
 dog.dig();
-//  action 3:
+//  action 3: the dog eats
 dog.eat();
-//  6)
+//  tree  6)
 //  Noun:
-//  action 1:
+//  action 1: the tree grows
 tree.grow();
-//  action 2:
+//  action 2: the tree branches
 tree.branch();
-//  action 3:
+//  action 3: the tree photosynthesizes
 tree.photosynthesize();
-//  7)
+//  taco truck  7)
 //  Noun:
-//  action 1:
+//  action 1: the taco truck makes tacos
 tacoTruck.makeTaco();
-//  action 2:
+//  action 2: the taco truck drives
 tacoTruck.drive();
-//  action 3:
+//  action 3: the taco truck makes change
 tacoTruck.makeChange();
-//  8)
+//  heater  8)
 //  Noun:
-//  action 1:
+//  action 1: the heater sets to high
 heater.high();
-//  action 2:
+//  action 2: the heater sets to medium
 heater.medium();
-//  action 3:
+//  action 3: the heater sets to low
 heater.low();
 //  9)
-//  Noun:
-//  action 1:
+//  Noun: air purifier
+//  action 1: the air purifier turns on
 airPurifier.on();
-//  action 2:
+//  action 2: the air purifier turns off
 airPurifier.off();
-//  action 3:
+//  action 3: the air purifier changes to the next speed
 airPurifier.nextSpeed();
-//  10)
+//   bird 10)
 //  Noun:
-//  action 1:
+//  action 1: the bird chirps
 bird.chirp();
-//  action 2:
+//  action 2: the bird flys
 bird.fly();
-//  action 3:
+//  action 3: the bird perches
 bird.perch();
 
 
